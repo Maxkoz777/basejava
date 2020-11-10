0ba4935d34965d5dae984f964f4051203d430cf3
Max <Maxkozhinov777@gmail.com>
2020-11-11 02:15:11 +0300

    Now on heroku: https://resumesdatabaseapplication.herokuapp.com/resume

--- src/com/urise/webapp/util/Config.java
+++ src/com/urise/webapp/util/Config.java
@@ -9,31 +9,32 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
-    public static String PROPS = "/resumes.properties";
+
     private final File storageDir;
     private final Storage storage;
-
-    private Config() {
-        try (InputStream inputStream = Config.class.getResourceAsStream(PROPS)) {
-            Properties properties = new Properties();
-            properties.load(inputStream);
-            storageDir = new File(properties.getProperty("storage.dir"));
-            storage = new SqlStorage(properties.getProperty("db.url"), properties.getProperty("db.user"), properties.getProperty("db.password"));
-        } catch (IOException e) {
-            throw new IllegalArgumentException("Invalid config file " + PROPS);
-        }
-    }
-
-    public Storage getStorage() {
-        return storage;
-    }
 
     public static Config getInstance() {
         return INSTANCE;
     }
 
+    private Config() {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
+            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS);
+        }
+    }
+
     public File getStorageDir() {
         return storageDir;
+    }
+
+    public Storage getStorage() {
+        return storage;
     }
 }
